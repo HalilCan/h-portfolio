e94cfcea21c3b836545538ec4978efb07662b034
HalilCan <halilcanmemoglu@gmail.com>
2018-09-05 07:24:59 -0400

    Changing things a bit

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -2,17 +2,17 @@
   "use strict"; // Start of use strict
 
   // Closes the sidebar menu
-  $(".menu-toggle").click(function(e) {
+  $(".menu-toggle").click = (e) => {
     e.preventDefault();
     $("#sidebar-wrapper").toggleClass("active");
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
     $(this).toggleClass("active");
-  });
+  };
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click = () => {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+      let target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
@@ -21,18 +21,18 @@
         return false;
       }
     }
-  });
+  };
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('#sidebar-wrapper .js-scroll-trigger').click(function() {
+  $('#sidebar-wrapper .js-scroll-trigger').click = () => {
     $("#sidebar-wrapper").removeClass("active");
     $(".menu-toggle").removeClass("active");
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-  });
+  };
 
   // Scroll to top button appear
   $(document).scroll(function() {
-    var scrollDistance = $(this).scrollTop();
+    let scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
     } else {
@@ -45,20 +45,22 @@
 // Disable Google Maps scrolling
 // See http://stackoverflow.com/a/25904582/1607849
 // Disable scroll zooming and bind back the click event
-var onMapMouseleaveHandler = function(event) {
-  var that = $(this);
+// noinspection JSUnusedLocalSymbols
+let onMapMouseleaveHandler = (event) => {
+  let that = $(this);
   that.on('click', onMapClickHandler);
   that.off('mouseleave', onMapMouseleaveHandler);
   that.find('iframe').css("pointer-events", "none");
-}
-var onMapClickHandler = function(event) {
-  var that = $(this);
+};
+// noinspection JSUnusedLocalSymbols
+let onMapClickHandler = (event) => {
+  let that = $(this);
   // Disable the click handler until the user leaves the map area
   that.off('click', onMapClickHandler);
   // Enable scrolling zoom
   that.find('iframe').css("pointer-events", "auto");
   // Handle the mouse leave event
   that.on('mouseleave', onMapMouseleaveHandler);
-}
+};
 // Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
