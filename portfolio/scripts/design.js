f96bb5d32dbd8bd7542bacb0edd20d84ac4adf05
HalilCan <halilcanmemoglu@gmail.com>
2018-10-04 23:05:10 -0400

    Testing out images

--- portfolio/scripts/design.js
+++ portfolio/scripts/design.js
@@ -29,7 +29,7 @@ function generateModal(elm) {
 
     let modalHeader = document.createElement("div");
     modalHeader.className = "modal-header";
-    modalHeader.appendChild(modalContent);
+    modalContent.appendChild(modalHeader);
     let closeButton = document.createElement("span");
     closeButton.className = "close";
     closeButton.value = "&times";
@@ -38,8 +38,22 @@ function generateModal(elm) {
     headerText.className = "header-text";
     modalHeader.appendChild(headerText);
 
+    let modalBody = document.createElement("div");
+    modalBody.className = "modal-body";
+    modalContent.appendChild(modalBody);
+    let modalBodyText = document.createElement("p");
+    modalBodyText.className = "modal-body-text";
+    modalBody.appendChild(modalBodyText);
+    let modalBodyText2 = document.createElement("p");
+    modalBodyText.className = "modal-body-text-2";
+    modalBody.appendChild(modalBodyText2);
 
-
+    let modalFooter = document.createElement("div");
+    modalFooter.className = "modal-footer";
+    modalContent.appendChild(modalFooter);
+    let modalFooterText = document.createElement("h3");
+    modalFooterText.className = "modal-footer-text";
+    modalFooter.appendChild(modalFooterText);
 
 
 
